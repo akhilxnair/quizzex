21457206a0340926756b6e4d8a9f907df32d0e22
Akhil Nair <4khiln4ir@gmail.com>
2020-07-12 09:11:11 +0530

    Updates

---
+++ precache-manifest.fc484c3676bef15507eeaf9176449ea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023c6c8798f7d7e2791d640074eb735c",
+    "url": "/quizzex/index.html"
+  },
+  {
+    "revision": "5f13ddb5bd8b508ed801",
+    "url": "/quizzex/static/js/2.cffb2a03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quizzex/static/js/2.cffb2a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cd005519256bea39c2",
+    "url": "/quizzex/static/js/main.e3be5812.chunk.js"
+  },
+  {
+    "revision": "126d48204544bfe7ed89",
+    "url": "/quizzex/static/js/runtime-main.b233f773.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/quizzex/static/media/nattu-adnan-unsplash.c11f97a3.jpg"
+  }
+]);
